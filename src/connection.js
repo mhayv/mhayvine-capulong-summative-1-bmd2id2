507a0755a7b79f2bcd3ecae6b3958cd18f1088b0
mhayv <mhayvine.capulong@mds.ac.nz>
2025-06-17 12:54:09 +1200

    removed previous data screens for now

--- src/connection.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://hzwalwwcuhouzlnyspza.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6d2Fsd3djdWhvdXpsbnlzcHphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxMDg4NzEsImV4cCI6MjA2NTY4NDg3MX0.UTjyoOUVmQSgxKvuBG9scx5MQyZjN1dCohqhvWdvhlg'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
